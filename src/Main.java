fdbbc8a6f0329684517d16bbf489c84537c1b438
Danilo Pivaral <DaniloPivaral@MacBook-Pro-2.local>
2023-08-29 20:27:09 -0400

    passed 4/5 tests

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        int[] test = {-5, -5, -5};
+        int[] test = {2, -5,-6, -8, 1, 2};
+
+        int[] test2 = {2, -100,-6, -8, 1, 2};
+        int[] test3 = {2, -5, -5};
+
 
         powerHungry powerHungry = new powerHungry();
         powerHungry.Solution(test);
+        powerHungry.Solution(test3);
     }
 }
--- src/powerHungry.java
+++ src/powerHungry.java
@@ -1,12 +1,34 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
 public class powerHungry {
 
-    public String Solution(int [] xs) {
+    public String Solution(int[] xs) {
         int negativeCount = 0;
+        int zeroCount = 0;
+        int sum = 0;
+
+        if(xs.length <= 1)
+        {
+            System.out.println(xs[0]);
+            return Integer.toString(xs[0]);
+        }
+
         List<Integer> xsList = new ArrayList<Integer>(xs.length);
         for (int o : xs)
         {
+            sum+= o;
+            if(o == 0)
+            {
+                zeroCount++;
+            }
             xsList.add(o);
+        }
+        if (zeroCount == xs.length)
+        {
+            System.out.println("0");
+            return "0";
         }
 
         Collections.sort(xsList);
@@ -35,6 +57,12 @@ public class powerHungry {
         } else {
             List<Integer> negatives = new ArrayList<>();
 
+            if(xs.length == 2)
+            {
+                System.out.println(Math.max(xs[0], xs[1]));
+                return Integer.toString(Math.max(xs[0], xs[1]));
+            }
+
             for(int negative: xsList)
             {
                 if(negative < 0)
@@ -42,9 +70,19 @@ public class powerHungry {
                 negatives.add(negative);
                 }
             }
+
             Collections.sort(negatives);
+
+            if(negatives.size() == 1)
+            {
+                System.out.println("0");
+                return "0";
+            }
+
             int lastElement = negatives.size() - 1;
             negatives.remove(lastElement);
+
+
 
             for (int j = 0; j < xsList.size(); j++) {
                 if (xsList.get(j) <= 0) {
@@ -62,7 +100,7 @@ public class powerHungry {
         }
 
 
-        System.out.println(String.valueOf(product));
+        System.out.println(Integer.valueOf(product).toString());
         return String.valueOf(product);
     }
 
